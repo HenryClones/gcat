919edb69a69cbd98e631740eb769e3bc494a208f
HenryClones <30604707+HenryClones@users.noreply.github.com>
2023-08-31 15:39:17 -0400

    Declare and use first functions

---
+++ gcat.h
@@ -0,0 +1,5 @@
+#ifndef GCAT_MANAGED_PAGE_SIZE
+#define GCAT_MANAGED_PAGE_SIZE 65536
+#endif
+
+void *malloc();
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <gcat.h>
+#include <assert.h>
+
+/**
+ * Main function which is the entrypoint for testing.
+ */
+int main(int argc, char **argv)
+{
+    void* fun_memory = malloc();
+    
+}
