5d4234db467ab08921049c269a7178566d6869c3
HenryClones <30604707+HenryClones@users.noreply.github.com>
2023-09-28 16:50:16 -0400

    Add auditing/message capacities to functions

--- include/wrappers.h
+++ include/wrappers.h
@@ -1,3 +1,5 @@
 int Open(char *path);
-int Close(int fd);
+int Close(int fd, char *name);
 void *Mmap(void *addr, size_t length, int fd);
+int Kill(char *cause, void *address);
+int Kill_offset(char *cause, void *address, int offset);
--- src/wrappers.c
+++ src/wrappers.c
@@ -1,5 +1,6 @@
 #include <sys/mman.h>
 #include <stdio.h>
+#include <signal.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
@@ -17,10 +18,32 @@
 
 #define MAP_FAILED ((void *) -1)
 
-static void unixerror(int n, char *stage, char *resource)
+// One string, given by the offending function.
+static void unixerror_simple(int n, char *cause)
+{
+    fprintf(stderr,
+        "GCAT error: Unix error %d during %s %s.\n", n, cause);
+}
+
+// Two strings, the stage/function and the resource.
+static void unixerror_citereason(int n, char *stage, char *resource)
 {
     fprintf(stderr,
         "GCAT error: Unix error %d during %s %s.\n", n, stage, resource);
+}
+
+// Two strings and a pointer, the causing function, the resource, and the offending pointer.
+static void unixerror_citeobject(int n, char *stage, char *resource, void *blame)
+{
+    fprintf(stderr,
+        "GCAT error: Unix error %d during %s at %p.\n", n, stage, resource, blame);
+}
+
+// Two strings and a pointer, the causing function, the resource, and the offending pointer.
+static void unixerror_citeoffset(int n, char *stage, char *resource, void *blame, int offset)
+{
+    fprintf(stderr,
+        "GCAT error: Unix error %d during %s at %p:%d.\n", n, stage, resource, blame, offset);
 }
 
 int Open(char *path)
@@ -28,17 +51,17 @@ int Open(char *path)
     int fd = open(path, O_RDWR);
     if (fd == -1)
     {
-        unixerror(errno, "initializing page by opening file path =", path);
+        unixerror_citereason(errno, "initializing page by opening file path ", path);
     }
     return fd;
 }
 
-int Close(int fd)
+int Close(int fd, char *name)
 {
     int result = close(fd);
     if (result == -1)
     {
-        unixerror(errno, "after initializing page closing ", "/dev/zero");
+        unixerror_citereason(errno, "after initializing page closing ", name);
     }
     return fd;
 }
@@ -49,7 +72,27 @@ void *Mmap(void *addr, size_t length, int fd)
         GCAT_MANAGED_PAGE_FLAGS, fd, 0);
     if (block == MAP_FAILED)
     {
-        unixerror(errno, "initializing page with", "mmap function");
+        unixerror_simple(errno, "initializing page with mmap function");
     }
     return block;
+}
+
+int Kill(char *cause, void *address)
+{
+    int status = kill(-1, SIGSEGV);
+    if (status == -1)
+    {
+        unixerror_citeobject(errno, "kill function causing SIGSEGV due to", cause, address);
+    }
+    return status;
+}
+
+int Kill_offset(char *cause, void *address, int offset)
+{
+    int status = kill(-1, SIGSEGV);
+    if (status == -1)
+    {
+        unixerror_citeoffset(errno, "kill function causing SIGSEGV due to", cause, address, offset);
+    }
+    return status;
 }
