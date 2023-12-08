2550164fba832596dd51f3b9bf1181ed740da65c
HenryClones <30604707+HenryClones@users.noreply.github.com>
2023-12-08 18:08:43 -0500

    Implementation of gcat can handle complex dependencies

--- tests/gcat_tests.c
+++ tests/gcat_tests.c
@@ -37,6 +37,18 @@ static int gcat_test01()
  */
 static int gcat_test02()
 {
+    uint64_t *data1 = gall(32, NULL);
+    uint64_t *data2 = gall(32, NULL);
+    if (data1 == data2)
+    {
+        return 1;
+    }
+    *data1 = 80;
+    *data2 = 81;
+    if (*data1 == *data2 || *data1 != *data2 - 1)
+    {
+        return 1;
+    }
     return 0;
 }
 
@@ -56,12 +68,76 @@ static int gcat_test04()
     return 0;
 }
 
+int finalizer_ran = 0;
+uint64_t set_value = 200;
+/**
+ * This function is a finalizer function meant for testing.
+ */
+static void finalizer(void *payload)
+{
+    finalizer_ran = ((uint64_t *)payload)[0] == set_value;
+}
+
 /**
  * Test gcat.h burr_stack.
  */
 static int gcat_test05()
 {
+    // Special case where =malloc/free
+    uint8_t *data[4];
+    data[0] = gall(32, NULL);
+    burr_stack(data[0]);
+    data[1] = gall(32, NULL);
+    if (data[0] != data[1])
+    {
+        return 1;
+    }
+    data[2] = gall(32, finalizer);
+    if (data[0] == data[2])
+    {
+        return 1;
+    }
+    if (finalizer_ran == 1)
+    {
+        return 1;
+    }
+    data[2][0] = 0;
+    burr_stack(data[2]);
+    if (finalizer_ran == 1)
+    {
+        return 1;
+    }
+    data[3] = gall(64, finalizer);
+    data[3][0] = set_value;
+    if (data[0] == data[3])
+    {
+        return 1;
+    }
+    if (finalizer_ran == 1)
+    {
+        return 1;
+    }
+    burr_stack(data[3]);
+    if (finalizer_ran == 0)
+    {
+        return 1;
+    }
     return 0;
+}
+
+int blocks = 5;
+int freed_tally = 0;
+static void true_finalizer(void *payload)
+{
+    // Assume it's NULL-pointer terminated
+    void **ptrs = payload;
+    void *current = NULL;
+    int i;
+    for (i = 0, current = ptrs[i]; current != NULL; current = ptrs[i])
+    {
+        burr_heap(current);
+    }
+    ++freed_tally;
 }
 
 /**
@@ -69,7 +145,40 @@ static int gcat_test05()
  */
 static int gcat_test06()
 {
-    return 0;
+    void **data[blocks];
+    // Triple pointers are hopefully going to solve all of my problems
+    int i;
+    for (i = 0; i < blocks; ++i)
+    {
+        // Fit the terminating null pointer
+        data[i] = gall(8 * (i + 2), true_finalizer);
+    }
+    // Do the setting as a separate step
+    for (i = 0; i < blocks; ++i)
+    {
+        int j;
+        for (j = 0; j < i; ++j)
+        {
+            if (i == 0)
+            {
+                data[i][j] = data[blocks - 1];
+            }
+            else
+            {
+                data[i][j] = data[i - 1];
+            }
+        }
+    }
+    // Build a really overcomplicated resource graph to challenge fate or w/e
+    for (i = 0; i < blocks; ++i)
+    {
+        if (freed_tally != 0)
+        {
+            return 1;
+        }
+        hew_stack(data[i]);
+    }
+    return freed_tally == blocks && data[0] == gall(64, NULL);
 }
 
 /**
